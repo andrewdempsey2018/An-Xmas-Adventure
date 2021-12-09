084a0ded41a5680fa3c2a110b504d464434ef7cc
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2021-12-09 22:44:09 +0000

    added platformer controls

--- assets/js/kaboom.js
+++ assets/js/kaboom.js
@@ -3,8 +3,8 @@ import kaboom from "https://unpkg.com/kaboom@next/dist/kaboom.mjs";
 
 // initialize kaboom context
 export const k = kaboom({
-    width: 800,
-    height: 600,
+    width: 640,
+    height: 480,
     background: [0, 51, 102]
 });
 
